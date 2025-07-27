3ba5ec50ca568d58f3be2ca501c58d3675752cfb
Kashif Ajmal <kashifchaudhary1413@gmail.com>
2025-07-27 18:53:15 +0500

    Seperate files

--- plugin.php
+++ plugin.php
@@ -3,27 +3,31 @@
 Plugin Name: TM Lorem Ipsum Generator
 Plugin URI:  https://github.com/kashif1413/WP-lorem-Ipsum-Generator-Plugin
 Description: A simple Lorem Ipsum generator tool.
-Version: 1.2
+Version: 1.3
 Author: Kashif Ajmal
+Date: 07/27/2025
 Author URI: https://github.com/kashif1413
 GitHub Plugin URI: kashif1413/WP-lorem-Ipsum-Generator-Plugin
 Primary Branch: main
 */
 
+// Enqueue styles and scripts only when shortcode is used
+function tm_lorem_enqueue_assets() {
+    // register
+    wp_register_style('tm-lorem-style', plugins_url('style.css', __FILE__));
+    wp_register_script('tm-lorem-script', plugins_url('script.js', __FILE__), array(), false, true);
+
+    // enqueue
+    wp_enqueue_style('tm-lorem-style');
+    wp_enqueue_script('tm-lorem-script');
+}
+add_action('wp_enqueue_scripts', 'tm_lorem_enqueue_assets');
+
 // Shortcode callback
 function tm_lorem_ipsum_tool() {
     ob_start();
-    ?>
-    <!-- Load Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@400;700&family=Roboto+Slab:wght@700&display=swap" rel="stylesheet">
-    
-    <?php
-    // Include your HTML file from the plugin directory
-    include plugin_dir_path(__FILE__) . 'lorem-ipsum-generator-tool.html';
-    ?>
-    <?php
+    // Include your HTML tool
+    include plugin_dir_path(__FILE__) . 'tool.html';
     return ob_get_clean();
 }
-
-// Register shortcode (shortcode name cannot have spaces!)
-add_shortcode('lorem_generate', 'tm_lorem_ipsum_tool');
+add_shortcode('lorem_generator', 'tm_lorem_ipsum_tool');
